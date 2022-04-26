dffd7f8e2ebf87e42075e52d47ac4890379a2f00
backtofayton <yucelolcay@gmail.com>
2022-04-26 10:50:23 +0300

    Update query interval

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 select * from animals where name ~ '.*(mon)';
-select * from animals where date_of_birth between '2016-01-01' and '2019-01-01';
+select * from animals where date_of_birth between '2016-01-01' and '2018-31-31';
 select * from animals where neutered = true and escape_attempts < 3;
 select date_of_birth from animals where name = 'Pikachu' or name= 'Agumon';
 select name, escape_attempts from animals where weight_kg > 10.5;
